3b31ec90daa8b347fbfac52b10c9c9e515b04666
Lhehe <45411926+Lhehe@users.noreply.github.com>
2019-12-20 16:51:23 +0800

    Update alvm.sh

--- alvm.sh
+++ alvm.sh
@@ -7,17 +7,17 @@ for((i=0;i<$num;i++));
 do
     read -p "请输入你想要个格式化的磁盘"  array_str[i]
 done
+echo "正在创建叫做VolGroup的逻辑卷组"
 pvcreate /dev/${array_str[0]}
-vgcreate VolGroup00 /dev/${array_str[0]}
+vgcreate VolGroup /dev/${array_str[0]}
 for((i=1;i<$num;i++));
 do
 pvcreate /dev/${array_str[i]}
-vgextend VolGroup00 /dev/${array_str[i]}
+vgextend VolGroup /dev/${array_str[i]}
 done
-echo "正在创建叫做VolGroup的逻辑卷组"
-echo "正在创建名为lv01的卷"
 Free_PE=`vgdisplay | grep "Free" | awk '{print $5}'`
-lvcreate -l +$Free_PE -n lv01 VolGroup00
+echo "正在创建名为lv01的卷"
+lvcreate -l +$Free_PE -n lv01 VolGroup
 LV_Path=`lvdisplay | grep "LV Path" | awk '{print $3}'`
 echo "请把 $LV_Path 格式化挂载使用,谢谢！！"
 else 
